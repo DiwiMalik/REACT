1f74251a000e9796097720a1f6e4703757e88c48
Diwi <diwimalik2003@gmail.com>
2023-06-26 00:07:18 +0530

    First Commit

---
+++ src/component/Basics/MenuApi.js
@@ -0,0 +1,110 @@
+/*const Menu = [
+    {
+        id:1
+        image: "images/bread omeltte.jpg",
+        name: "Bread Omelette",
+        category: "Breakfast",
+        price: "Rs.160",
+        description :"Bread omelette is a popular and satisfying breakfast dish that combines the elements of omelette and bread. It is a quick and easy-to-make recipe that requires minimal ingredients and can be customized to suit individual tastes.",
+    },
+    {
+        id:2,
+       image: "images/fruit bowl.jpg",
+        name: "Fruit Bowl",
+        category: "Breakfast",
+        price: "Rs.320",
+        description :"The bowl is brimming with an abundance of vibrant and succulent fruits, creating a tantalizing display of nature's colorful palette. A medley of aromas wafts from the bowl, enticing the senses with the promise of freshness and sweetness.",
+    },
+    {
+        id:3,
+        image: "images/idli.jpg",
+        name: "Idli",
+        category: "Breakfast",
+        price: "Rs.210",
+        description :"The idlis, the star of the dish, are soft, fluffy, and mildly tangy. They are made by fermenting a batter composed of ground rice and urad dal (a type of lentil) along with water and a pinch of salt. The batter is left to ferment overnight, resulting in a light and airy texture. The idlis are then steamed in special molds, which give them their distinct round shape.",
+    },
+    {
+        id:4,
+        image: "images/masala dosa.jpg",
+        name: "Masala Dosa",
+        category: "Breakfast",
+        price: "Rs.220",
+        description :"Masala dosa is a popular and iconic South Indian dish that consists of a thin, crispy crepe-like pancake made from fermented rice and lentil batter. The dosa is typically filled with a spiced potato mixture and served with chutneys and sambar, creating a delicious and satisfying meal.",
+    },
+    {
+        id:5,
+       image: "images/Poha.jpg",
+        name: "Poha",
+        category: "Breakfast",
+        price: "Rs.190",
+        description :" Poha, also known as flattened rice or beaten rice, is a popular and traditional breakfast dish in India. It is made from rice that has been parboiled, flattened, and dried, resulting in thin, translucent flakes. Poha is a versatile ingredient and forms the base for various savory preparations.",
+    },
+    {
+        id:6,
+       image: "images/parantha.jpg",
+        name: "Parantha",
+        category: "Breakfast",
+        price: "Rs.110",
+        description :"Parantha, also spelled as paratha, is a popular and versatile Indian flatbread that is enjoyed as a main course or a snack. It is made by kneading whole wheat flour with water and other ingredients to form a smooth dough, which is then rolled out into thin circles and cooked on a hot griddle or tawa.",
+    },
+];
+export default Menu */
+const Menu = [
+    {
+      id: 1,
+      image: "images/breadomelette.jpg",
+      name: "Bread Omelette",
+      category: "Breakfast",
+      price: "160₹",
+      description:
+        "Bread omelette is a popular and satisfying breakfast dish that combines the elements of omelette and bread. It is a quick and easy-to-make recipe that requires minimal ingredients and can be customized to suit individual tastes. ",
+    },
+  
+    {
+      id: 2,
+      image: "images/fruitbowl.jpg",
+      name: "Fruit Bowl",
+      category: "Breakfast",
+      price: "200₹",
+      description:
+        "The bowl is brimming with an abundance of vibrant and succulent fruits, creating a tantalizing display of nature's colorful palette. A medley of aromas wafts from the bowl, enticing the senses with the promise of freshness and sweetness.",
+    },
+    {
+      id: 3,
+      image: "images/idli.jpg",
+      name: "Idli",
+      category: "Lunch",
+      price: "150₹",
+      description:
+        "The idlis, the star of the dish, are soft, fluffy, and mildly tangy. They are made by fermenting a batter composed of ground rice and urad dal (a type of lentil) along with water and a pinch of salt. The batter is left to ferment overnight, resulting in a light and airy texture. The idlis are then steamed in special molds, which give them their distinct round shape.",
+    },
+    {
+      id: 4,
+      image: "../images/poha.jpg",
+      name: "Poha",
+      category: "Lunch",
+      price: "120₹",
+      description:
+        "Poha, also known as flattened rice or beaten rice, is a popular and traditional breakfast dish in India. It is made from rice that has been parboiled, flattened, and dried, resulting in thin, translucent flakes. Poha is a versatile ingredient and forms the base for various savory preparations.",
+    },
+    {
+      id: 5,
+      image: "../images/parantha.jpg",
+      name: "Parantha",
+      category: "Dinner",
+      price: "100₹",
+      description:
+        "Parantha, also spelled as paratha, is a popular and versatile Indian flatbread that is enjoyed as a main course or a snack. It is made by kneading whole wheat flour with water and other ingredients to form a smooth dough, which is then rolled out into thin circles and cooked on a hot griddle or tawa.",
+    },
+    {
+      id: 6,
+      image: "../images/masaladosa.jpg",
+      name: "Masala Dosa",
+      category: "Dinner",
+      price: "180₹",
+      description:
+        "Masala dosa is a popular and iconic South Indian dish that consists of a thin, crispy crepe-like pancake made from fermented rice and lentil batter. The dosa is typically filled with a spiced potato mixture and served with chutneys and sambar, creating a delicious and satisfying meal.",
+    },
+  ];
+  
+  export default Menu;
---
+++ src/component/Navbar.js
@@ -0,0 +1,49 @@
+/*import React from 'react'
+
+const Navbar = ({filterItem, menuList}) => {
+  return (
+    <>
+      <nav className="navbar">
+      <div className="btn-group">
+        {
+            menuList.map((curElem)=>{
+                return (
+                    <button className="btn-group_item"
+                    onClick={()=>filterItem("curElem")}>
+                     {curElem}
+                      </button>
+                       )
+            })
+        }
+       
+      </div>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar*/
+
+import React from "react";
+
+const Navbar = ({ filterItem, menuList }) => {
+  return (
+    <>
+      <nav className="Navbar">
+        <div className="btn-group">
+          {menuList.map((curElem) => {
+            return (
+              <button
+                className="btn-group__item"
+                onClick={() => filterItem(curElem)}>
+                {curElem}
+              </button>
+            );
+          })}
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
